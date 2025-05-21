0f9f0e6e11ff66098d1c651f486cbf3c3d24d33a
DG <99335589+dogerber@users.noreply.github.com>
2025-05-21 15:11:02 +0200

    feat: implemented <OpenWeatherOneCall.h> v4.0.3; images still broken

--- credentials_example.h
+++ credentials_example.h
@@ -4,23 +4,22 @@
 // Fill out with your information and rename file to credentials.h
 
 // Change to your wifi ssid and password
-#define SSID ""
-#define PASS ""
+char SSID[] = "";
+char PASS[] = "";
 
-// Openweather API key (with OneCall Subscription!)
-char *APIKEY = "";
+// Openweather API key
+char ONECALLKEY[] = "";
 
-// the location to be used
-float myLatitude = 40.39;
-float myLongitude = 10.49;
+float myLATITUDE = 1.1; // I got this from Wikipedia
+float myLONGITUDE = 2.1;
 
 // the time zone to be used
-int timeZone = 2; //
+int timeZone = 4; //
 
 // google calendar (or any url that downloads an .ics (iCal) of the calendar)
 // go to Calendar/Settings/Private adress in iCal format
-char *calendarURL[] = {""};  // seperate by comma for multiple
-const int num_Calendars = 3; // how many of the above to be used
+char *calendarURL[] = {"url1", "url2", "url3"}; // seperate by comma for multiple
+const int num_Calendars = 3;                    // how many of the above to be used
 
 const char *calNames[] = {"cal1", "cal2", "cal3"}; // this will be displayed before the calendar entries in bold, can be empty
 
